beb9178019c0be762e994e537a35910b820c5aa3
Nitin <nksinghal41@gmail.com>
2020-08-30 10:10:17 +0530

    log file

--- src/main/java/io/nitinLearn/ratinginfoservice/controller/HomeController.java
+++ src/main/java/io/nitinLearn/ratinginfoservice/controller/HomeController.java
@@ -1,10 +1,20 @@
 package io.nitinLearn.ratinginfoservice.controller;
 
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.nitinLearn.ratinginfoservice.service.HomeService;
+
+
 @RestController
 public class HomeController {
+	
+	@Autowired
+	HomeService homeService;
+	
+	private final Logger log = Logger.getLogger(this.getClass());
 	
 	@GetMapping(value = "/")
 	public String getTest() {
---
+++ src/main/java/io/nitinLearn/ratinginfoservice/service/HomeService.java
@@ -0,0 +1,8 @@
+package io.nitinLearn.ratinginfoservice.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HomeService {
+
+}
