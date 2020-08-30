6b603bc31a7f34c41535b8a32f78ae8e3e256675
Nitin <nksinghal41@gmail.com>
2020-08-30 11:31:34 +0530

    db connection

--- src/main/java/io/nitinLearn/ratinginfoservice/controller/HomeController.java
+++ src/main/java/io/nitinLearn/ratinginfoservice/controller/HomeController.java
@@ -3,8 +3,10 @@ package io.nitinLearn.ratinginfoservice.controller;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.nitinLearn.ratinginfoservice.bean.Rating;
 import io.nitinLearn.ratinginfoservice.service.HomeService;
 
 
@@ -18,6 +20,14 @@ public class HomeController {
 	
 	@GetMapping(value = "/")
 	public String getTest() {
+		log.info("fetcing");
 		return "ratingInfo";
+	}
+	
+	@GetMapping(value="/rating/{movieId}")
+	public Rating getRating(@PathVariable String movieId) {
+		log.info("fetcing  rating "+movieId);
+
+		return homeService.getRating(movieId);
 	}
 }
--- src/main/java/io/nitinLearn/ratinginfoservice/service/HomeService.java
+++ src/main/java/io/nitinLearn/ratinginfoservice/service/HomeService.java
@@ -1,8 +1,20 @@
 package io.nitinLearn.ratinginfoservice.service;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
+
+import io.nitinLearn.ratinginfoservice.bean.Rating;
 
 @Service
 public class HomeService {
+	
+
+	private final Logger log = Logger.getLogger(this.getClass());
+
+	public Rating getRating(String movieId) {
+		// TODO Auto-generated method stub
+		log.info("called into rating service "+movieId);
+		return new Rating(movieId , 5);
+	}
 
 }
